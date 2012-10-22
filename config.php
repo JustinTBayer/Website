56fb15963a0963ffeeac797ffe5b0c67c6dfa6f4
Logan McCamon <logan@mccamon.org>
2012-10-22 12:06:41 -0500

    defined config.php

--- config.php
+++ config.php
@@ -2,15 +2,13 @@
 
 /** Define database constants */
 /** MySQL database name */
-define('DB_NAME', '');
+define('DB_NAME', 'acm');
 
 /** MySQL database username */
-define('DB_USER', '');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', '');
-
-?>
+define('DB_HOST', 'localhost');
